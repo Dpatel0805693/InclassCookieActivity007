eb1ab7bc82fb4a9f6933b48b56de7c194416a14a
Michelle-l33 <michelle.lee@spartans.ut.edu>
2024-11-19 11:30:53 -0500

    [doc] - comments

--- remove-favorites.php
+++ remove-favorites.php
@@ -1,9 +1,9 @@
 <?php
-session_start();
-if(isset($_GET['PaintingID'])){
+session_start(); #adds to session
+if(isset($_GET['PaintingID'])){ #if the query has a specific painting id
     $paintingID = $_GET['PaintingID'];
 
-    foreach($_SESSION['favorites'] as $index =>$favorite){
+    foreach($_SESSION['favorites'] as $index =>$favorite){ #go through favories list and remove the painting that was specified
         if ($favorite['PaintingID']==$paintingID){
             unset($_SESSION['favorites'][$index]);
         $_SESSION['favorites'] = array_values($_SESSION['favorites']); // Re-index the array
@@ -11,8 +11,8 @@ if(isset($_GET['PaintingID'])){
         }
     }
 }
-else{
+else{ #if there was not a specific painting ID, then the remove all button was clicked so reset entire favorites list
     $_SESSION['favorites'] = [];
 }
-header('Location: view-favorites.php');
+header('Location: view-favorites.php'); #go back to view favorites page
 exit();
